b18b2680d91cb6244fd6d149f2341d2eb05e7a47
Gianfranco Palumbo <gianpa@gmail.com>
2020-04-06 23:20:07 +0100

    fix(e2e test): /login route

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -3,6 +3,6 @@
 describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/login');
-    cy.contains('h1', 'Login');
+    cy.contains('h3', 'Login to save your channels 📺');
   });
 });
